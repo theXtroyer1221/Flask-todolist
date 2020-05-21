a42bd9ce0613fc9c269d03ee3a83012fd5213e39
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-05-21 18:38:09 +0200

    started the socket.io live chat page

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired, Length, AnyOf
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "todolist"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
+socketio = SocketIO(app)
 
 
 class InputForm(FlaskForm):
@@ -24,15 +26,20 @@ class Todo(db.Model):
         return f'Todo({self.id}, {self.content})'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template("index.html")
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
     form = InputForm()
     if form.validate_on_submit():
         todoinput = Todo(content=form.todo.data)
         db.session.add(todoinput)
         db.session.commit()
     todos = Todo.query.all()
-    return render_template("index.html", form=form, todos=todos)
+    return render_template("todo.html", form=form, todos=todos)
 
 
 @app.route('/update', methods=['GET', 'POST'])
@@ -51,8 +58,36 @@ def update():
     else:
         todo.complete = False
     db.session.commit()
-    return render_template("index.html", form=form, todos=todos)
+    return render_template("todo.html", form=form, todos=todos)
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+    form = InputForm()
+    if form.validate_on_submit():
+        todoinput = Todo(content=form.todo.data)
+        db.session.add(todoinput)
+        db.session.commit()
+    todos = Todo.query.all()
+    print(request.form.to_dict())
+    todo_id = request.form.to_dict()["delete"]
+    print(todo_id)
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return render_template("todo.html", form=form, todos=todos)
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+
+@app.route('/chat')
+def chat():
+    return render_template("chat.html")
 
 
 if "__main__" == __name__:
-    app.run(debug=True)
+    socketio.run(app, debug=True)
